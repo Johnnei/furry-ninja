b3766799be8d9abc66f4237910a5d6671269aa65
Johnnei <johnnei07@live.nl>
2012-05-10 22:09:13 +0200

    I will Start with the game itself before the menus

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,10 +1,90 @@
 package engine;
 
+import game.WormsGame;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+import static org.lwjgl.opengl.GL11.*;
+
 public class Game {
 
+	private WormsGame game;
+	
+	/**
+	 * Creates a new instance of the Game Engine
+	 */
+	public Game() {
+		game = new WormsGame();
+	}
+	
+	/**
+	 * Runs the entire game until close will be requested or crashes
+	 */
 	public void run() {
-		// TODO Auto-generated method stub
+		initOpenGL();
+		long lastTick = getCurrentMillis();
 		
+		while(!Display.isCloseRequested()) {
+			
+			//Game Logic Section
+			while(getCurrentMillis() - lastTick >= 50) { //20 ticks per second
+				lastTick += 50;
+				game.onTick();
+			}
+			
+			//Game Render Section
+			game.render();
+			
+			//LWJGL Updating
+			Display.update();
+			Display.sync(60);
+		}
+	}
+	
+	/**
+	 * Gets the current time in millis by dividing the current nano time by 100'000
+	 * @return
+	 */
+	public static long getCurrentMillis() {
+		return System.nanoTime() / 1000000;
+	}
+	
+	public static void sleep(long ms) {
+		System.out.println("Sleeping for " + ms + "ms");
+		long timeSlept = System.currentTimeMillis();
+		try {
+			Thread.sleep(ms);
+		} catch (InterruptedException e) {
+			timeSlept = System.currentTimeMillis() - timeSlept;
+			if((ms - timeSlept) > 0)
+				sleep(ms - timeSlept);
+		}
+	}
+	
+	/**
+	 * Initialising the display and OpenGL basic setup
+	 */
+	private void initOpenGL() {
+		try {
+			//Init Display
+			Display.setDisplayMode(new DisplayMode(1280, 720));
+			Display.setTitle("Furry Ninja");
+			Display.setVSyncEnabled(true);
+			Display.create();
+			
+			//Init OpenGL Frame
+			glMatrixMode(GL_PROJECTION);
+	        glLoadIdentity();
+	        glOrtho(0, 1280, 720, 0, 1, -1);
+	        glMatrixMode(GL_MODELVIEW);
+	        glLoadIdentity();
+			
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
 	}
 
 }
---
+++ src/game/WormsGame.java
@@ -0,0 +1,16 @@
+package game;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class WormsGame {
+	
+	
+	public void onTick() {
+		
+	}
+	
+	public void render() {
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+
+}
