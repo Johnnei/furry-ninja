5c1c5554d70531e71b1c47331947f8442d3e798a
Johnnei <johnnei07@live.nl>
2012-05-10 21:22:59 +0200

    Configurating Eclipse Workspace and Libs

---
+++ src/engine/Game.java
@@ -0,0 +1,10 @@
+package engine;
+
+public class Game {
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/Launcher.java
@@ -0,0 +1,12 @@
+package main;
+
+import engine.Game;
+
+public class Launcher {
+	
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.run();
+	}
+
+}
